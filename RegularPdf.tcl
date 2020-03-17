cef6d11243033337b606d8b82a61de54921e0193
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2020-03-18 00:33:32 +0100

    Added customized listbox

--- RegularPdf.tcl
+++ RegularPdf.tcl
@@ -17,11 +17,20 @@ label .mTop.1Label -text {A PDF Authoring Tool}
 label .mTop.2Label -text "\u00a9 2020 Abdullah Fatota" -font {TkDefaultFont 10 italic}
 pack .mTop.0Label .mTop.1Label .mTop.2Label -side top -pady 10 -padx 2cm
 
+# List box
+listbox .mList -relief flat -highlightthickness 2 \
+-highlightcolor black -cursor hand2
+pack .mList -side left -expand 0 -fill y 
+.mList config -highlightbackground [.mList cget -highlightcolor] ; # highlight background -> When NOT in Focus
+
 button .mButton -text {Button 1} -bg #123456 -fg white
 place .mButton -relx 0.5 -rely 0.5 -anchor center
 
+# Root Menu
 menu .mMenu
+. config -menu .mMenu
+
+# Help->About Menu
 menu .mMenu.mHelp
 .mMenu.mHelp add command -command {wm deicon .mTop} -label About
-. config -menu .mMenu
 .mMenu add cascade -label Help -menu .mMenu.mHelp
