6e01718e4d136513fee0150410f892676caf076a
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2020-03-17 22:47:15 +0100

    modified About dialog

--- RegularPdf.tcl
+++ RegularPdf.tcl
@@ -13,8 +13,9 @@ wm withdraw .mTop
 wm title .mTop About
 wm protocol .mTop WM_DELETE_WINDOW {wm withdraw .mTop}
 label .mTop.0Label -text [wm title .] -font {Tahoma 16 normal}
-label .mTop.1Label -text "\u00a9 2020 Abdullah Fatota" -font {TkDefaultFont 10 italic}
-pack .mTop.0Label .mTop.1Label -side top -pady 10 -padx 2cm
+label .mTop.1Label -text {A PDF Authoring Tool}
+label .mTop.2Label -text "\u00a9 2020 Abdullah Fatota" -font {TkDefaultFont 10 italic}
+pack .mTop.0Label .mTop.1Label .mTop.2Label -side top -pady 10 -padx 2cm
 
 button .mButton -text {Button 1} -bg #123456 -fg white
 place .mButton -relx 0.5 -rely 0.5 -anchor center
