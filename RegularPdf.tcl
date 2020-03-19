ea01417ee14cbf18bdb02b8ef2d409f335d46aa5
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2020-03-19 02:04:09 +0100

    New naming 'convention' further custom listbox mega-widget

--- RegularPdf.tcl
+++ RegularPdf.tcl
@@ -17,11 +17,27 @@ label .mTop.1Label -text {A PDF Authoring Tool}
 label .mTop.2Label -text "\u00a9 2020 Abdullah Fatota" -font {TkDefaultFont 10 italic}
 pack .mTop.0Label .mTop.1Label .mTop.2Label -side top -pady 10 -padx 2cm
 
+# List box Frame
+set a [frame .0frame -relief groove -borderwidth 2]
+
+pack $a -side left -expand false -fill y
+# Current Items Label
+set b [label .0frame.0label -text {Items in current directory}]
+# Reload Button
+set c [button .0frame.0button -text Reload]
+# Separator
+set d [ttk::separator .0frame.0separator -orient horizontal]
+set dd [ttk::separator .0frame.1separator -orient horizontal]
+# Pack
+pack $b $d -fill x
+pack $c -anchor ne -pady 2
+pack $dd -fill x
 # List box
-listbox .mList -relief flat -highlightthickness 2 \
--highlightcolor black -cursor hand2
-pack .mList -side left -expand 0 -fill y 
-.mList config -highlightbackground [.mList cget -highlightcolor] ; # highlight background -> When NOT in Focus
+set e [listbox $a.0list -relief flat -highlightthickness 2 -highlightcolor black -cursor hand2]
+pack $e -side left -fill y 
+#$e config -highlightbackground [$e cget -highlightcolor] ; # highlight background -> When NOT in Focus
+$e config -background [. cget -background]
+#bind .mList <Visibility> {puts "Visibility event fired >%s<"}
 
 button .mButton -text {Button 1} -bg #123456 -fg white
 place .mButton -relx 0.5 -rely 0.5 -anchor center
