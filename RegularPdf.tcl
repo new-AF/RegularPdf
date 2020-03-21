9cc5db70ab9bd077cccc5b46c009ec0c675d36a8
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2020-03-21 02:15:34 +0100

    + Unicode "icons" + lisbox hover effect

--- RegularPdf.tcl
+++ RegularPdf.tcl
@@ -17,46 +17,94 @@ label .mTop.1Label -text {A PDF Authoring Tool}
 label .mTop.2Label -text "\u00a9 2020 Abdullah Fatota" -font {TkDefaultFont 10 italic}
 pack .mTop.0Label .mTop.1Label .mTop.2Label -side top -pady 10 -padx 2cm
 
-
+set Font TkDefaultFont
+set UnicodeFolder "\ud83d\udcc2"
+set UnicodeBack "\u2190"
+set UnicodeReload "\u21ba"
+set boldfont {-font {-weight bold}}
 
 # List box Frame
-set a [frame .0frame -relief groove -borderwidth 2]
-pack $a -side left -expand false -fill y
+set a [labelframe .0frame -relief groove -borderwidth 2 -text "Items in current directory" -relief solid]
+pack $a -side left -expand false -fill y -padx 5 -pady 5
 # Current Items Label
-set b [label .0frame.0label -text {Items in current directory}]
+#set b [label .0frame.0label ]
 # Reload Button
-set c [button .0frame.0button -text Reload]
+set c [button .0frame.0button -text $UnicodeReload]
+#puts [$c configure]
 # Separator
 set d [ttk::separator .0frame.0separator -orient horizontal]
 set dd [ttk::separator .0frame.1separator -orient horizontal]
 # Pack
-pack $b $d -fill x
-pack $c -anchor ne -pady 2
+pack $d -fill x
+pack $c -anchor ne
 pack $dd -fill x
 # List box
-set e [listbox $a.0list -relief flat -highlightthickness 2 -highlightcolor black -cursor hand2 -activestyle dotbox]
-pack $e -side left -fill y 
+set e [listbox $a.0list -relief flat -highlightthickness 2 -highlightcolor blue -cursor hand2 -activestyle dotbox -selectmode single]
+set f [listbox $a.1list -relief flat -highlightthickness 2 -highlightcolor red -cursor hand2 -activestyle dotbox -bg [. cget -bg]]
+pack $e -side right -expand 1 -fill both
+pack $f -side left -expand 1 -fill y
+proc Adjustf {} {
+	
+	set p [font measure TkDefaultFont tes]
+	$::f config -width 3; #"${p}p"
+	puts "yyyyyyyyyyyy $p"
+}
 #$e config -highlightbackground [$e cget -highlightcolor] ; # highlight background -> When NOT in Focus
 $e config -background [. cget -background]
 
 # Bind Button
 proc get_files {{path ""}} { ; # gets directories too.
-	global e
+	
 	if {$path == ""} {
 		set path [pwd]
 	}
-	$e delete 0 end
+	$::e delete 0 end
 	set files [glob -directory $path *]
-	puts $files
+	#puts $files
+	$::e insert 0 $::UnicodeBack
+	$::f insert 0 ""
 	foreach f $files {
-		$e insert end [lindex [file split $f] end] 
+		$::e insert end [lindex [file split $f] end] 
+		$::f insert end $::UnicodeFolder 
 	}
 	if {[string is alpha %s] == 1} {
 		bind $e <Visibility> ""
 	}
 }
-$c config -command "get_files"
-bind $e <Visibility> "get_files"
+
+
+$c config -command get_files
+bind $e <Visibility> {get_files ; Adjustf}
+
+set eHover {}
+proc hover {e y} {
+	set i [$e nearest $y]
+	
+	if {$::eHover != $i} {
+		if {$::eHover != ""} {$e itemconfig $::eHover -background [$e cget -background]}
+		set ::eHover $i
+	}
+	
+	if { [$e curselection] != $i } {
+		$e itemconfigure $i -bg yellow
+	}
+}
+bind $e <Motion> {
+	
+	hover $::e %y
+	
+	
+}
+
+
+bind $e <Leave> {
+	set i [$::e nearest %y]
+	if {[$::e curselection] != $i} {
+		$::e itemconfig $i -background [$::e cget -background]
+	}
+	#puts $item
+}
+
 #bind .mList <Visibility> {puts "Visibility event fired >%s<"}
 
 button .mButton -text {Button 1} -bg #123456 -fg white
@@ -70,3 +118,5 @@ menu .mMenu
 menu .mMenu.mHelp
 .mMenu.mHelp add command -command {wm deicon .mTop} -label About
 .mMenu add cascade -label Help -menu .mMenu.mHelp
+
+console show
